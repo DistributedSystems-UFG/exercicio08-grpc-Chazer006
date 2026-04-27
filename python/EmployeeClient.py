577579b060e041f492210552ed26a524a2b2f307
Pedro Lucas <102874758+Chazer006@users.noreply.github.com>
2026-04-26 23:01:38 -0300

    Refactor EmployeeClient to use new service endpoints
    Updated employee management functionality with new endpoints for listing, retrieving, and counting employees.

--- python/EmployeeClient.py
+++ python/EmployeeClient.py
@@ -1,35 +1,24 @@
 from __future__ import print_function
 import logging
-
 import grpc
 import EmployeeService_pb2
 import EmployeeService_pb2_grpc
-
 import const
 
 def run():
     with grpc.insecure_channel(const.IP+':'+const.PORT) as channel:
         stub = EmployeeService_pb2_grpc.EmployeeServiceStub(channel)
 
-        # Query an employee's data
-        response = stub.GetEmployeeDataFromID(EmployeeService_pb2.EmployeeID(id=101))
-        print ('Employee\'s data: ' + str(response))
-
-        # Add a new employee
-        response = stub.CreateEmployee(EmployeeService_pb2.EmployeeData(id=301, name='Jose da Silva', title='Programmer'))
-        print ('Added new employee ' + response.status)
-
-        # Change an employee's title
-        response = stub.UpdateEmployeeTitle(EmployeeService_pb2.EmployeeTitleUpdate(id=301, title='Senior Programmer'))
-        print ('Updated employee ' + response.status)
-
-        # Delete an employee
-        response = stub.DeleteEmployee(EmployeeService_pb2.EmployeeID(id=201))
-        print ('Deleted employee ' + response.status)
-
-        # List all employees
+        print("--- Testando Endpoints Originais ---")
         response = stub.ListAllEmployees(EmployeeService_pb2.EmptyMessage())
-        print ('All employees: ' + str(response))
+        print('Funcionários atuais: ' + str(len(response.employee_data)))
+
+        print("\n--- Testando Novos Endpoints ---")
+        name_res = stub.GetEmployeeName(EmployeeService_pb2.EmployeeID(id=101))
+        print('Nome do funcionário 101: ' + name_res.name)
+
+        count_res = stub.CountEmployees(EmployeeService_pb2.EmptyMessage())
+        print('Total de funcionários no DB: ' + str(count_res.count))
 
 if __name__ == '__main__':
     logging.basicConfig()
